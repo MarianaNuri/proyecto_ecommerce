cfad7f06a499e3f388ac1d506cfa54557fad7566
MarianaNuri <l24270017@tuxtla.tecnm.mx>
2026-02-20 21:16:10 -0600

    Archivo de "quienes" y archivo de JS

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    console.log("JS conectado correctamente ;)");
+/* Código para el botón "Ver más" en la sección de historia  de quienes.html */
+    const boton = document.getElementById("btn-ver-mas");
+    const info = document.getElementById("extra-info");
+
+    if (boton && info) {
+        boton.addEventListener("click", function() {
+            info.classList.toggle("oculto");
+            if (info.classList.contains("oculto")) {
+                boton.textContent = "Ver más";
+            } else {
+                boton.textContent = "Ver menos";
+            }
+        });
+    }
+});
