b903743bcc3bc2b99119d27f0576d235e2fe6e40
Eva del Carmen Gonzalez Santiz <L24270005@tuxtla.tecnm.mx>
2026-02-21 15:33:52 -0600

    Actualizar script.js

--- assets/script.js
+++ assets/script.js
@@ -56,3 +56,79 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+
+/* Registro */
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("registroForm");
+    const btnEnviar = document.getElementById("btnEnviar");
+    const checkbox = document.getElementById("terminos");
+    const mensajeError = document.getElementById("mensajeError");
+
+    if (form && btnEnviar && checkbox) {
+
+        checkbox.addEventListener("change", function () {
+            btnEnviar.disabled = !this.checked;
+        });
+
+        form.addEventListener("submit", function (event) {
+            if (!form.checkValidity()) {
+                event.preventDefault();
+                mensajeError.textContent = "Complete todos los campos correctamente";
+            } else {
+                mensajeError.textContent = "";
+                alert("Registro exitoso");
+            }
+        });
+    }
+});
+
+/*Busqueda de productos*/
+document.addEventListener("DOMContentLoaded", function () {
+    const productos = [
+        { nombre: "Suculenta", precio: 120, imagen: "imagenes/suculenta.jpg" },
+        { nombre: "Monstera", precio: 250, imagen: "imagenes/monstera.jpg" },
+        { nombre: "Cactus", precio: 90, imagen: "imagenes/cactus.jpg" },
+        { nombre: "Helecho", precio: 180, imagen: "imagenes/helecho.jpg" }
+    ];
+    const params = new URLSearchParams(window.location.search);
+    const busqueda = params.get("q");
+
+    const contenedor = document.getElementById("resultados");
+    const input = document.getElementById("inputBusqueda")
+    const titulo = document.getElementById("tituloBusqueda")
+
+    if (busqueda){
+        //mostra la palabra en el input
+        if (input) {
+            input.value = busqueda;
+        }
+        //cambia el titulo dinamicamente
+        if (titulo) {
+            titulo.textContent = `Resultados de busqueda de "${busqueda}"`;
+        }
+        if (contenedor) {
+            const filtrados = productos.filter(producto =>
+                producto.nombre.toLowerCase().includes(busqueda.toLowerCase())
+            );
+
+            if (filtrados.length > 0) {
+                filtrados.forEach(producto => {
+                    contenedor.innerHTML += `
+                        <div class="col-md-4">
+                            <div class="card shadow-sm">
+                                <img src="${producto.imagen}" class="card-img-top">
+                                <div class="card-body">
+                                    <h5 class="card-title">${producto.nombre}</h5>
+                                    <p class="card-text">$${producto.precio}</p>
+                                </div>
+                            </div>
+                        </div>
+                    `;
+                });
+            } else {
+                contenedor.innerHTML = `<p>No se encontraron productos.</p>`;
+            }
+        }
+    }
+
+});
